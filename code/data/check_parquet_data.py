59e1123341d5fd41fba5daac353cd062865cbcd3
root <windszzlang@gmail.com>
2025-03-15 16:50:22 +0000

    update

--- code/data/check_parquet_data.py
+++ code/data/check_parquet_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-path = '/home/azureuser/cloudfiles/code/DeepRetrieval/code/data/sql/bird/train.parquet'
+# path = '/home/azureuser/cloudfiles/code/DeepRetrieval/code/data/sql/bird/train.parquet'
+path = '/home/azureuser/cloudfiles/code/DeepRetrieval/code/data/sql/bird/test.parquet'
 df = pd.read_parquet(path)
 print(f"Loaded {len(df)} rows")
 print(df.head(3))
