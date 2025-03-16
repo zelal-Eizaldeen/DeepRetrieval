fa83b19e1b1b94550b76ce1380980ade7348b54b
linjc16 <250551019@qq.com>
2025-03-16 14:54:00 -0500

    add dense nfcorpus scifact

--- code/src/eval/Dense/baselines/search/scifact.py
+++ code/src/eval/Dense/baselines/search/scifact.py
@@ -15,7 +15,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default="scifact", help="Dataset to evaluate")
-    parser.add_argument('--res_path', type=str, default="../results_dense/Qwen-inst-scifact.json", help="Path to the qrels file")
+    parser.add_argument('--res_path', type=str, default="../results_dense/claude-3.5_post_scifact.json", help="Path to the qrels file")
     args = parser.parse_args()
 
     model_path = "intfloat/multilingual-e5-large-instruct"
