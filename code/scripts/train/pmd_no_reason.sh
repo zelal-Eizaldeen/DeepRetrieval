4c73c7f91230e7deb49b289040301128754d3bba
pat-jj <pj20@illinois.edu>
2025-03-25 17:40:06 -0500

    some updates

--- code/scripts/train/pmd_no_reason.sh
+++ code/scripts/train/pmd_no_reason.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=3,4
 
 DATE=$(date '+%Y-%m-%d-%H-%M-%S')
 
@@ -14,10 +14,10 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.ppo_mini_batch_size=16 \
     actor_rollout_ref.actor.ppo_micro_batch_size=8 \
     critic.ppo_micro_batch_size=8 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=2 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.model.enable_gradient_checkpointing=True \
@@ -27,13 +27,13 @@ python3 -m verl.trainer.main_ppo \
     trainer.logger=['wandb'] \
     +trainer.val_before_train=False \
     trainer.default_hdfs_dir=null \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
-    trainer.save_freq=150 \
-    trainer.test_freq=150 \
+    trainer.save_freq=100 \
+    trainer.test_freq=500 \
     trainer.project_name=pubmed_search \
     trainer.experiment_name=pubmed_search_no_reason \
-    actor_rollout_ref.model.path=/shared/eng/pj20/lmr_model/pubmed_search_3b_no_reason/actor/global_step_200 \
-    critic.model.path=/shared/eng/pj20/lmr_model/pubmed_search_3b_no_reason/critic/global_step_200 \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-3B-Instruct \
+    critic.model.path=Qwen/Qwen2.5-3B-Instruct\
     trainer.default_local_dir=/shared/eng/pj20/lmr_model/pubmed_search_3b_no_reason \
-    trainer.total_epochs=5 2>&1 | tee exp_log/3b-ppo-verl_demo_$DATE.log 
+    trainer.total_epochs=15 2>&1 | tee exp_log/3b-ppo-verl_demo_$DATE.log 
