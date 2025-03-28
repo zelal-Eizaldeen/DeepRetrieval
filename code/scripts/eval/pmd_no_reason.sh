dd6b1980413db0b2074b000b8fc1e09aec60b2fc
pat-jj <pj20@illinois.edu>
2025-03-28 13:13:21 -0500

    some updates

--- code/scripts/eval/pmd_no_reason.sh
+++ code/scripts/eval/pmd_no_reason.sh
@@ -1,6 +1,6 @@
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=6
 
 python src/eval/SearchEngine/eval_pubmed.py \
-    --model_path /shared/eng/pj20/lmr_model/pubmed_search_3b_no_reason/actor/global_step_700 \
+    --model_path /shared/eng/pj20/lmr_model/pubmed_search_3b_no_reason/actor/global_step_900 \
     --data_path data/search_engine/no_reason/SearchEngine/pmd_no_reason/test_full.parquet \
-    --model_name pubmed_no_reason_700
+    --model_name pubmed_no_reason_900
